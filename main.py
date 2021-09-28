dc8261dd308548591ac8233cf582478ad0731819
Aidancartney24 <824776@mystma.org>
2021-09-28 14:32:51 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def passWord(p):
+    if p == "Knights19":
+        return "ACCESS GRANTED"
+
+    else:
+        return "ACCESS DENIED"
+
+print(passWord("Knights19"))
